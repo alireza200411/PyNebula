c944fa6545d7595cc4bc9762978c39a4614e8659
alireza200411 <alirezamoradi292048@gmail.com>
2025-03-17 21:16:41 +0330

    Adding medium first-class codes

--- function/firstclass.py
+++ function/firstclass.py
@@ -37,3 +37,37 @@ name_list = ['john', 'tom', 'william', 'james', 'oliver', 'henry']
 x = list(map(lambda name: name.upper(), name_list))
 
 # ----> Task 06 <----
+main_list = [1, 6, 89, 91, 56]
+newlist = list(map(lambda num: num + 100, main_list))
+
+
+# ----> Task 07 <----
+def operations_math(func, number_list):
+    answer_list = [func(number) for number in number_list]
+    return answer_list
+
+# ----> Task 08 <----
+operators_dic = {
+    'add': lambda x, y: x + y,
+    'subtraction': lambda x, y: x - y,
+    'multiplication': lambda x, y: x * y,
+    'division': lambda x, y: x // y,
+    'power': lambda x, y: x ** y,
+
+}
+
+def math(operator, num1, num2):
+    if operator in operators_dic:
+        return operators_dic[operator](num1, num2)
+    return "Error: Invalid operator"
+
+# ----> Task 09 <----
+def sum_of_calculations(list_, literal):
+    return sum([func(literal) for func in list_ ]) 
+
+# operators_list = [lambda x: x + 10, lambda x: x - 10, lambda x: x * 10, lambda x: x // 10, lambda x: x ** 3]
+# ----> Task 10 <----
+def bool_list(func, list_: list):
+    return [item if func(item) else None for item in list_]
+
+

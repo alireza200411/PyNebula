0cc56feb7c327da3ebfe72d5e47e223f924ac8f3
alireza200411 <alirezamoradi292048@gmail.com>
2025-03-24 18:46:00 +0330

    Adding simple lambda code

---
+++ function/lambda.py
@@ -0,0 +1,32 @@
+# ----> Task 01 <----
+double = lambda a: 2 * a
+
+# ----> Task 02 <----
+sum_lambda = lambda b, c: b + c
+
+# ----> Task 03 <----
+abs = lambda d: -1 * d if d < 0 else d
+
+# ----> Task 04 <----
+evenOrOdd = lambda x: "odd" if x % 2 == 1 else "even"
+
+# ----> Task 05 <----
+findMax = lambda x, y: x if x > y else y
+
+# ----> Task 06 <----
+PowerOfTwo = lambda x: x ** 2
+
+# ----> Task 07 <----
+full_name = lambda name, lastname: name + lastname
+
+# ----> Task 08 <----
+lentgh = lambda string: len(string)
+
+# ----> Task 09 <----
+ifChar = lambda string, charakter: True if charakter in string else False
+
+# ----> Task 10 <----
+capital = lambda phrase: phrase.capitaliza()
+
+
+
